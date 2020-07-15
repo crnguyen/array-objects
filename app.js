ca4c9890f39cd8757f8f1e024b5af2f08cd3a1e6
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-07-15 14:03:50 -0700

    [arrays] add food

---
+++ app.js
@@ -0,0 +1,13 @@
+var name = "Cristina";
+console.log(name);
+
+var city = "SF";
+console.log(city);
+
+var favNum = 9;
+console.log(favNum);
+
+var food = ['pho', 'pizza', 'sushi', 'chicken'];
+console.log(food);
+
+//console.log stores info that we can access later
