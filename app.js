50d9aa40d4604d256e8bba3dd7265b731972171c
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-07-15 14:22:40 -0700

    [loops] add while loops

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ var food = ['pho', 'pizza', 'sushi', 'chicken'];
 console.log(food);
 
 //console.log stores info that we can access later
+//while loops
+var number = 10;
+var otherNumber = 1;
+while (otherNumber < number) {
+    console.log(otherNumber);
+    otherNumber += 1; //other number = other number + 1 - considered a counter
+}
+
+var numberTwo = 20;
+var myOtherNumber = 10;
+while (myOtherNumber < numberTwo) {
+    console.log(myOtherNumber); //prints 10, 11
+    myOtherNumber += 1; //prints 11, 12 ---- adding 1 each time the loop runs
+}
